b82ce1cb702c0e2cf7106feb88c286c41375def4
Valon Saidi <vsaidi33@gmail.com>
2022-12-20 22:28:36 +0100

    Fixed a bog in port enviorment variable

--- backend/server.js
+++ backend/server.js
@@ -2,29 +2,29 @@ const mongoose = require("mongoose");
 require("dotenv").config({ path: "./.env" });
 
 process.on("uncaughtException", (err) => {
-  console.log("Uncaught Exception - Server is shuting down...");
-  console.log(err);
-  console.log(err.message);
-  process.exit(1);
+    console.log("Uncaught Exception - Server is shuting down...");
+    console.log(err);
+    console.log(err.message);
+    process.exit(1);
 });
 
 const app = require("./app");
 
 mongoose
-  .connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("DB connection successful!"));
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB connection successful!"));
 
-const server = app.listen(process.env.PORT || 500, () => {
-  console.log(`App listening on port: ${process.env.PORT || 500}`);
+const server = app.listen(process.env.PORT || 5000, () => {
+    console.log(`App listening on port: ${process.env.PORT || 5000}`);
 });
 
 process.on("unhandledRejection", (err) => {
-  console.log("UnHandled Rejaction - Server is shuting down...");
-  console.log(err.message);
-  server.close(() => {
-    process.exit(1);
-  });
+    console.log("UnHandled Rejaction - Server is shuting down...");
+    console.log(err.message);
+    server.close(() => {
+        process.exit(1);
+    });
 });
