9d376608b8740b62e93e6c0acd9b3bb697237887
Valon Saidi <vsaidi33@gmail.com>
2023-01-18 16:52:25 +0100

    fixing a bug ...

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,90 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import {
-    IoRestaurantSharp,
-    IoStarOutline,
-    IoTimeOutline,
+  IoRestaurantSharp,
+  IoStarOutline,
+  IoTimeOutline,
 } from "react-icons/io5";
 import { TiArrowForward } from "react-icons/ti";
 // import { useGlobalContext } from "../../Context/context";
 import { useSelector, useDispatch } from "react-redux";
 import {
-    openModal,
-    likeRecipe,
-    unLikeRecipe,
+  openModal,
+  likeRecipe,
+  unLikeRecipe,
 } from "../../features/util/utilSlice";
 
 const Card = ({
-    category,
-    recipeTitle,
-    numberPeople,
-    prepTime,
-    shortDescription,
-    likes,
-    photo,
-    liked,
-    id,
+  category,
+  recipeTitle,
+  numberPeople,
+  prepTime,
+  shortDescription,
+  likes,
+  photo,
+  liked,
+  id,
 }) => {
-    // const { openModal, likeRecipe, unlikeRecipe } = useGlobalContext();
-    const { myRecipes, loading } = useSelector((state) => state.recipe);
-    const dispatch = useDispatch();
-    const baseUrl = "https://recipe-app-backend-4xd6.onrender.com";
+  // const { openModal, likeRecipe, unlikeRecipe } = useGlobalContext();
+  const { allRecipes, myRecipes, loading } = useSelector(
+    (state) => state.recipe
+  );
+  const [modalRecipe, setModalRecipe] = useState({});
+  const dispatch = useDispatch();
+  const baseUrl = "https://recipe-app-backend-4xd6.onrender.com";
 
-    const toggleLikeRecepies = () => {
-        if (liked === false) {
-            // likeRecipe(id);
-        } else if (liked === true) {
-            // unlikeRecipe(id);
-        }
-    };
+  const toggleLikeRecepies = () => {
+    if (liked === false) {
+      dispatch(likeRecipe(id));
+    } else if (liked === true) {
+      dispatch(unLikeRecipe(id));
+    }
+  };
 
-    return (
-        <div className="card">
-            <div className="card__img">
-                <span className="badge green card__badge">{category}</span>
-                <img
-                    src={
-                        photo
-                            ? `${baseUrl}/api/v1/upload/${photo}`
-                            : "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/08/20/0/FNM_100120-Ultimate-Beef-Stew_s4x3.jpg.rend.hgtvcom.406.305.suffix/1597931275143.jpeg"
-                    }
-                    // src={
-                    //   photo
-                    //     ? `./images/${photo}`
-                    //     : "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/08/20/0/FNM_100120-Ultimate-Beef-Stew_s4x3.jpg.rend.hgtvcom.406.305.suffix/1597931275143.jpeg"
-                    // }
-                    className="card-img-top"
-                    alt="card img"
-                />
-            </div>
-            <div className="card-body">
-                <h5 className="card-title">{recipeTitle}</h5>
-                <p className="card-text">
-                    {shortDescription && shortDescription.slice(0, 200)}
-                </p>
-            </div>
-            <div className="card--footer">
-                <div className="card--icons">
-                    <span>
-                        <IoTimeOutline /> {prepTime} min
-                    </span>
-                    <span>
-                        <IoRestaurantSharp /> {numberPeople} person
-                    </span>
-                    <span>
-                        <IoStarOutline
-                            onClick={toggleLikeRecepies}
-                            style={{ cursor: "pointer" }}
-                            className={liked ? `like--blue` : ""}
-                        />{" "}
-                        {likes?.length}
-                    </span>
-                </div>
-                <button className=" green card--btn">
-                    <TiArrowForward onClick={() => dispatch(openModal(id))} />
-                </button>
-            </div>
+  const prov = () => {
+    const recepie = allRecipes.filter((rec) => rec._id === id);
+    setModalRecipe(recepie[0]);
+    dispatch(openModal(id, modalRecipe));
+  };
+
+  return (
+    <div className="card">
+      <div className="card__img">
+        <span className="badge green card__badge">{category}</span>
+        <img
+          src={
+            photo
+              ? `${baseUrl}/api/v1/upload/${photo}`
+              : "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/08/20/0/FNM_100120-Ultimate-Beef-Stew_s4x3.jpg.rend.hgtvcom.406.305.suffix/1597931275143.jpeg"
+          }
+          className="card-img-top"
+          alt="card img"
+        />
+      </div>
+      <div className="card-body">
+        <h5 className="card-title">{recipeTitle}</h5>
+        <p className="card-text">
+          {shortDescription && shortDescription.slice(0, 200)}
+        </p>
+      </div>
+      <div className="card--footer">
+        <div className="card--icons">
+          <span>
+            <IoTimeOutline /> {prepTime} min
+          </span>
+          <span>
+            <IoRestaurantSharp /> {numberPeople} person
+          </span>
+          <span>
+            <IoStarOutline
+              onClick={toggleLikeRecepies}
+              style={{ cursor: "pointer" }}
+              className={liked ? `like--blue` : ""}
+            />{" "}
+            {likes?.length}
+          </span>
         </div>
-    );
+        <button className=" green card--btn">
+          <TiArrowForward onClick={prov} />
+          {/* <TiArrowForward onClick={() => dispatch(openModal(id))} /> */}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- client/src/features/recipes/recipeSlice.js
+++ client/src/features/recipes/recipeSlice.js
@@ -4,182 +4,183 @@ import _ from "lodash";
 const baseUrl = "http://localhost:5000";
 
 export const getRecipes = createAsyncThunk("recipe/getrecipes", async () => {
-    return await API.get(`/api/v1/recipes`);
+  return await API.get(`/api/v1/recipes`);
 });
 
 export const createRecipe = createAsyncThunk(
-    "recipe/createrecipe",
-    async ({
-        recipe,
-        recipeTitle,
-        category,
-        prepTime,
-        shortDescription,
-        numberPeople,
-        author,
-        photo,
-    }) => {
-        return await API.post(`/api/v1/recipes`, {
-            recipe,
-            recipeTitle,
-            category,
-            prepTime,
-            shortDescription,
-            numberPeople,
-            author,
-            photo,
-        });
-    }
+  "recipe/createrecipe",
+  async ({
+    recipe,
+    recipeTitle,
+    category,
+    prepTime,
+    shortDescription,
+    numberPeople,
+    author,
+    photo,
+  }) => {
+    return await API.post(`/api/v1/recipes`, {
+      recipe,
+      recipeTitle,
+      category,
+      prepTime,
+      shortDescription,
+      numberPeople,
+      author,
+      photo,
+    });
+  }
 );
 
 export const getRecipe = createAsyncThunk("recipe/getrecipe", async (id) => {
-    return await API.get(`/api/v1/recipes/${id}`);
+  return await API.get(`/api/v1/recipes/${id}`);
 });
 
 export const getMyRecipes = createAsyncThunk(
-    "recipe/getmyrecipes",
-    async () => {
-        return await API.get(`/api/v1/recipes/myrecipes`);
-    }
+  "recipe/getmyrecipes",
+  async () => {
+    return await API.get(`/api/v1/recipes/myrecipes`);
+  }
 );
 
 export const updateRecipe = createAsyncThunk(
-    "recipe/updaterecipe",
-    async (recipe) => {
-        return await API.patch(`/api/v1/recipes/${recipe.id}`, {
-            ...recipe,
-        });
-    }
+  "recipe/updaterecipe",
+  async (recipe) => {
+    return await API.patch(`/api/v1/recipes/${recipe.id}`, {
+      ...recipe,
+    });
+  }
 );
 
 export const deleteRecipe = createAsyncThunk(
-    "recipe/deleterecipe",
-    async (id) => {
-        return await API.delete(`/api/v1/recipes/${id}`);
-    }
+  "recipe/deleterecipe",
+  async (id) => {
+    return await API.delete(`/api/v1/recipes/${id}`);
+  }
 );
+export const shareRecipe = (state) => {
+  return state.allRecipes;
+};
 
 const initialState = {
-    loading: false,
-    allRecipes: [],
-    myRecipes: [],
-    recipe: {},
-    createdRecipe: {},
-    error: [],
-    newestRecipes: [],
-    mostLikedRecipes: [],
-    sortedRecipes: [],
+  loading: false,
+  allRecipes: [],
+  myRecipes: [],
+  recipe: {},
+  createdRecipe: {},
+  error: [],
+  newestRecipes: [],
+  mostLikedRecipes: [],
+  sortedRecipes: [],
 };
 
 export const recipeSlice = createSlice({
-    name: "recipe",
-    initialState,
-    reducers: {
-        mostLiked: (state) => {
-            state.mostLikedRecipes = _.sortBy(state?.allRecipes, [
-                "likes",
-            ]).reverse();
-        },
-        newest: (state) => {
-            state.newestRecipes = _.sortBy(state?.allRecipes, ["createdAt"])
-                .reverse()
-                .slice(0, 3);
-        },
-        sorted: (state, action) => {
-            console.log("action", action);
-            state.sortedRecipes = state.allRecipes.filter(
-                (recepis) => recepis.category === action.payload
-            );
-        },
+  name: "recipe",
+  initialState,
+  reducers: {
+    mostLiked: (state) => {
+      state.mostLikedRecipes = _.sortBy(state?.allRecipes, ["likes"]).reverse();
     },
-
-    extraReducers: (builder) => {
-        builder
-            .addCase(getRecipes.pending, (state) => {
-                state.loading = true;
-            })
-            .addCase(getRecipes.fulfilled, (state, action) => {
-                state.loading = false;
-                console.log(action);
-                state.allRecipes = action.payload.data.data.recipe;
-            })
-            .addCase(getRecipes.rejected, (state, action) => {
-                state.loading = false;
-                state.allRecipes = [];
-                state.error = action.error.message;
-            });
-
-        builder
-            .addCase(createRecipe.pending, (state) => {
-                state.loading = true;
-            })
-            .addCase(createRecipe.fulfilled, (state, action) => {
-                state.loading = false;
-                console.log("Created Recipe", action.payload.data.data);
-                state.createdRecipe = action.payload;
-            })
-            .addCase(createRecipe.rejected, (state, action) => {
-                state.loading = false;
-                state.error = action.error.message;
-            });
-
-        builder
-            .addCase(getRecipe.pending, (state) => {
-                state.loading = true;
-            })
-            .addCase(getRecipe.fulfilled, (state, action) => {
-                state.loading = false;
-                state.recipe = action.payload;
-            })
-            .addCase(getRecipe.rejected, (state, action) => {
-                state.loading = false;
-                state.recipe = null;
-                state.error = action.error.message;
-            });
-
-        builder
-            .addCase(getMyRecipes.pending, (state) => {
-                state.loading = true;
-            })
-            .addCase(getMyRecipes.fulfilled, (state, action) => {
-                state.loading = false;
-                state.myRecipes = action.payload.data.data.recipes;
-            })
-            .addCase(getMyRecipes.rejected, (state, action) => {
-                state.loading = false;
-                state.myRecipes = [];
-                state.error = action.error.message;
-            });
-
-        builder
-            .addCase(updateRecipe.pending, (state) => {
-                state.loading = true;
-            })
-            .addCase(updateRecipe.fulfilled, (state, action) => {
-                state.loading = false;
-                state.recipe = action.payload;
-            })
-            .addCase(updateRecipe.rejected, (state, action) => {
-                state.loading = false;
-                state.recipe = {};
-                state.error = action.error.message;
-            });
-
-        builder
-            .addCase(deleteRecipe.pending, (state) => {
-                state.loading = true;
-            })
-            .addCase(deleteRecipe.fulfilled, (state, action) => {
-                state.myRecipes = state.myRecipes.filter(
-                    (rec) => rec._id !== action.meta.arg
-                );
-                state.loading = false;
-            })
-            .addCase(deleteRecipe.rejected, (state, action) => {
-                state.loading = false;
-                state.error = action.error.message;
-            });
+    newest: (state) => {
+      state.newestRecipes = _.sortBy(state?.allRecipes, ["createdAt"])
+        .reverse()
+        .slice(0, 3);
     },
+    sorted: (state, action) => {
+      console.log("action", action);
+      state.sortedRecipes = state.allRecipes.filter(
+        (recepis) => recepis.category === action.payload
+      );
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(getRecipes.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getRecipes.fulfilled, (state, action) => {
+        state.loading = false;
+        console.log(action);
+        state.allRecipes = action.payload.data.data.recipe;
+      })
+      .addCase(getRecipes.rejected, (state, action) => {
+        state.loading = false;
+        state.allRecipes = [];
+        state.error = action.error.message;
+      });
+
+    builder
+      .addCase(createRecipe.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(createRecipe.fulfilled, (state, action) => {
+        state.loading = false;
+        console.log("Created Recipe", action.payload.data.data);
+        state.createdRecipe = action.payload;
+      })
+      .addCase(createRecipe.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+
+    builder
+      .addCase(getRecipe.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getRecipe.fulfilled, (state, action) => {
+        state.loading = false;
+        state.recipe = action.payload;
+      })
+      .addCase(getRecipe.rejected, (state, action) => {
+        state.loading = false;
+        state.recipe = null;
+        state.error = action.error.message;
+      });
+
+    builder
+      .addCase(getMyRecipes.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getMyRecipes.fulfilled, (state, action) => {
+        state.loading = false;
+        state.myRecipes = action.payload.data.data.recipes;
+      })
+      .addCase(getMyRecipes.rejected, (state, action) => {
+        state.loading = false;
+        state.myRecipes = [];
+        state.error = action.error.message;
+      });
+
+    builder
+      .addCase(updateRecipe.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(updateRecipe.fulfilled, (state, action) => {
+        state.loading = false;
+        state.recipe = action.payload;
+      })
+      .addCase(updateRecipe.rejected, (state, action) => {
+        state.loading = false;
+        state.recipe = {};
+        state.error = action.error.message;
+      });
+
+    builder
+      .addCase(deleteRecipe.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(deleteRecipe.fulfilled, (state, action) => {
+        state.myRecipes = state.myRecipes.filter(
+          (rec) => rec._id !== action.meta.arg
+        );
+        state.loading = false;
+      })
+      .addCase(deleteRecipe.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
 });
 export const { mostLiked, newest, sorted } = recipeSlice.actions;
 
