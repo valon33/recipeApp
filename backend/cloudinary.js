b01c323699faf3f5afbe098a9478adfeed934818
Valon Saidi <vsaidi33@gmail.com>
2023-09-19 00:57:49 +0200

    added publick id 2

--- backend/cloudinary.js
+++ backend/cloudinary.js
@@ -38,8 +38,4 @@ cloudinary.config({
         console.error('Error uploading image:', error);
       }
     };
-
-
-    cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(result); });
+    
