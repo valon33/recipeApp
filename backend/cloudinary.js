dc2ab66c6ccc35ddb11b546eee5328fcec52ed14
Valon Saidi <vsaidi33@gmail.com>
2023-09-19 21:37:11 +0200

    photo naem fixed2

--- backend/cloudinary.js
+++ backend/cloudinary.js
@@ -10,7 +10,7 @@ cloudinary.config({
   exports.fileUploadCloudinary = async (req, res) => {
     try {
         // const bufferData = req.data
-        console.log("wdadsfreaqwe", req.files.photo.data.name);
+        console.log("wdadsfreaqwe", req);
         const bufferData = req.files.photo.data
     
         // Upload the Buffer as an image
